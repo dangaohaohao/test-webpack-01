ba8ec5324f61a0fed5cbe46beae379efff43cf7d
刘玉珍 <liuyuzhen@MacBook-Pro.local>
2020-09-01 18:50:20 +0800

    feat: 功能模块设计

---
+++ build-webpack/lib/webpack.base.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const ROOT_PATH = path.resolve(__dirname);
+const ENTRY_FILE_REG = /src\/(.*)\/index\.js/;
+
+const setMPA = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+
+  const entryFiles = glob.sync(path.resolve(ROOT_PATH, './src/*/index.js'));
+
+  entryFiles.map((entryFile) => {
+    const match = entryFile.match(ENTRY_FILE_REG);
+    const pageName = match && match[1];
+
+    entry[pageName] = entryFile;
+    htmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `/src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: ['vendors', pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+        },
+      }),
+    );
+  });
+
+  return {
+    entry,
+    htmlWebpackPlugins,
+  };
+};
+
+const { entry, htmlWebpackPlugins } = setMPA();
+
+module.exports = {
+  entry: entry,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        include: path.resolve(__dirname, './src'),
+        exclude: path.resolve(__dirname, './node_modules'),
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1100000,
+              name: '[name]_[hash:8].[ext]',
+              outputPath: 'assets/',
+              publicPath: 'assets/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new FriendlyErrorsWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css',
+    }),
+    function () {
+      this.hooks.done.tap('done', (stats) => {
+        if (
+          stats.compilation.errors &&
+          stats.compilation.errors.length &&
+          process.argv.indexOf('--watch') == -1
+        ) {
+          process.exit(1);
+        }
+      });
+    },
+  ].concat(htmlWebpackPlugins),
+};
---
+++ build-webpack/lib/webpack.ssr.js
@@ -0,0 +1,57 @@
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const {
+  merge
+} = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+
+const ssrConfig = {
+  mode: 'production',
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: 'ignore-loader',
+      },
+      {
+        test: /\.less$/,
+        use: 'ignore-loader',
+      },
+    ],
+  },
+  plugins: [
+    new OptimizeCssAssetsWebpackPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [{
+          module: 'react',
+          entry: 'https://unpkg.com/react@16/umd/react.production.min.js',
+          global: 'React',
+        },
+        {
+          module: 'react-dom',
+          entry: 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js',
+          global: 'ReactDom',
+        },
+      ],
+    }),
+  ],
+  devtool: 'none',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: true,
+      cacheGroups: {
+        commons: {
+          name: 'commons',
+          chunks: 'all',
+          minChunks: 2,
+        },
+      },
+    },
+  },
+};
+
+module.exports = merge(baseConfig, ssrConfig);
